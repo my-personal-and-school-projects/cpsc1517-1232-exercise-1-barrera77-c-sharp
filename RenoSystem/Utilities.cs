2786a6afcd717c8d2596848d27c9b01d90fcace1
Manuel Alva <barrera_ml@hotmail.com>
2024-01-26 00:15:34 -0700

    Rename Variables and add Utilities.cs class

---
+++ RenoSystem/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenoSystem
+{
+    public class Utilities
+    {
+        public static bool IsNonZeroPositive(int value) => value > 0;
+
+        public static bool IsValidMinimum(int value1, int value2) => value1 >= value2;
+
+        public static bool IsZeroPositive(int value) => value >= 0;
+    }
+}
